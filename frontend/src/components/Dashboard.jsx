5cf6f3f7aa9170a4f4551d382877e9406b3ac61b
kevinazuero <kazueror@unemi.edu.ec>
2025-11-22 17:44:13 -0500

    more changes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://practica-semana-14.onrender.com/api';
 
 function Dashboard({ token, setToken }) {
   const [usuarios, setUsuarios] = useState([]);
--- frontend/src/components/Registro.jsx
+++ frontend/src/components/Registro.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://practica-semana-14.onrender.com//api';
 
 function Registro({ setVista }) {
   const [datos, setDatos] = useState({ nombre: '', email: '', password: '', edad: '' });
